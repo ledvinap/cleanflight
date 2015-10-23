a40b074cf244a0725d8cee08e8a37dc92ddf30d4
Petr Ledvina <ledvinap@gmail.com>
2015-10-23 13:09:12 +0200

    Implement priority handling for EXTI
    Conflicts:
    	src/main/drivers/exti.c

--- src/main/drivers/exti.c
+++ src/main/drivers/exti.c
@@ -17,12 +17,24 @@ typedef struct {
 
 extiChannelRec_t extiChannelRecs[16];
 
-#ifdef STM32F10X
-static uint8_t extiIRQn[] = {
+// IRQ gouping, same on 103 and 303
+#define EXTI_IRQ_GROUPS 7
+//                                      0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15
+static const uint8_t extiGroups[16] = { 0, 1, 2, 3, 4, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6 };
+static uint8_t extiGroupPriority[EXTI_IRQ_GROUPS];
+
+#if defined(STM32F10X)
+static const uint8_t extiGroupIRQn[EXTI_IRQ_GROUPS] = {
     EXTI0_IRQn, EXTI1_IRQn, EXTI2_IRQn, EXTI3_IRQn, EXTI4_IRQn,
-    EXTI9_5_IRQn, EXTI9_5_IRQn, EXTI9_5_IRQn, EXTI9_5_IRQn, EXTI9_5_IRQn,
-    EXTI15_10_IRQn, EXTI15_10_IRQn, EXTI15_10_IRQn, EXTI15_10_IRQn, EXTI15_10_IRQn, EXTI15_10_IRQn
+    EXTI9_5_IRQn, EXTI15_10_IRQn
 };
+#elif defined(STM32F303xC)
+static const uint8_t extiGroupIRQn[EXTI_IRQ_GROUPS] = {
+    EXTI0_IRQn, EXTI1_IRQn, EXTI2_TS_IRQn, EXTI3_IRQn, EXTI4_IRQn,
+    EXTI9_5_IRQn, EXTI15_10_IRQn
+};
+#else
+# warning "Unknown CPU"
 #endif
 
 void EXTIInit(void)
@@ -38,6 +50,7 @@ void EXTIInit(void)
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
 #endif
     memset(extiChannelRecs, 0, sizeof(extiChannelRecs));
+    memset(extiGroupPriority, 0xff, sizeof(extiGroupPriority));
 }
 
 void EXTIHandlerInit(extiCallbackRec_t *self, extiHandlerCallback *fn)
@@ -52,6 +65,8 @@ void EXTIConfig(IO_t io, extiCallbackRec_t *cb, int irqPriority, EXTITrigger_Typ
     if(chIdx < 0)
         return;
     extiChannelRec_t *rec = &extiChannelRecs[chIdx];
+    int group = extiGroups[chIdx];
+
     rec->handler = cb;
 #if defined(STM32F10X)
     GPIO_EXTILineConfig(IO_GPIO_PortSource(io), IO_GPIO_PinSource(io));
@@ -71,12 +86,29 @@ void EXTIConfig(IO_t io, extiCallbackRec_t *cb, int irqPriority, EXTITrigger_Typ
     EXTIInit.EXTI_LineCmd = ENABLE;
     EXTI_Init(&EXTIInit);
 
-    NVIC_InitTypeDef NVIC_InitStructure;
-    NVIC_InitStructure.NVIC_IRQChannel = extiIRQn[chIdx];
-    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = NVIC_PRIORITY_BASE(irqPriority);
-    NVIC_InitStructure.NVIC_IRQChannelSubPriority = NVIC_PRIORITY_SUB(irqPriority);
-    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-    NVIC_Init(&NVIC_InitStructure);
+    if(extiGroupPriority[group] > irqPriority) {
+        extiGroupPriority[group] = irqPriority;
+
+        NVIC_InitTypeDef NVIC_InitStructure;
+        NVIC_InitStructure.NVIC_IRQChannel = extiGroupIRQn[group];
+        NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = NVIC_PRIORITY_BASE(irqPriority);
+        NVIC_InitStructure.NVIC_IRQChannelSubPriority = NVIC_PRIORITY_SUB(irqPriority);
+        NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+        NVIC_Init(&NVIC_InitStructure);
+    }
+}
+
+void EXTIRelease(IO_t io)
+{
+    // don't forget to match cleanup with config
+    EXTIEnable(io, false);
+
+    int chIdx;
+    chIdx = IO_GPIOPinIdx(io);
+    if(chIdx < 0)
+        return;
+    extiChannelRec_t *rec = &extiChannelRecs[chIdx];
+    rec->handler = NULL;
 }
 
 void EXTIEnable(IO_t io, bool enable)
@@ -90,10 +122,10 @@ void EXTIEnable(IO_t io, bool enable)
     else
         EXTI->IMR &= ~extiLine;
 #elif defined(STM32F303xC)
-    int extiLine = IO_EXTILine(io);
+    int extiLine = IO_EXTI_Line(io);
     if(extiLine < 0)
         return;
-    // assume extiLine < 32 (walid for all EXTI pins)
+    // assume extiLine < 32 (valid for all EXTI pins)
     if(enable)
         EXTI->IMR |= 1 << extiLine;
     else
